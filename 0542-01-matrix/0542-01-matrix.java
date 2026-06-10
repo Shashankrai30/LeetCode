ec36b7d3d812e4086023c138c84540b041487983
Shashank Rai <164053591+Shashankrai30@users.noreply.github.com>
2026-06-11 00:38:30 +0530

    Time: 13 ms (91.13%), Space: 49.5 MB (25.16%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,50 @@
+class Solution {
+    public static class pair{
+        int row;
+        int col;
+        int level;
+        public pair(int row,int col,int level){
+            this.row=row;
+            this.col=col;
+            this.level=level;
+        }
+    }
+    public static int dr[]={-1,1,0,0};
+    public static int dc[]={0,0,1,-1};
+    public static int rowl;
+    public static int coll;
+    public int[][] updateMatrix(int[][] mat) {
+        rowl=mat.length;
+        coll=mat[0].length;
+        Queue<pair> q=new LinkedList<>();
+        boolean visited[][]=new boolean[rowl][coll];
+
+        for(int i=0;i<rowl;i++){
+            for(int j=0;j<coll;j++){
+                if(mat[i][j]==0){
+                    q.offer(new pair(i,j,0));
+                    visited[i][j]=true;
+                }
+            }
+        }
+        
+        while(!q.isEmpty()){
+            pair curr=q.poll();
+            int r=curr.row;
+            int c=curr.col;
+            int level=curr.level;
+
+            for(int i=0;i<4;i++){
+                int nrow=r+dr[i];
+                int ncol=c+dc[i];
+
+                if(nrow>=0 && ncol>=0 && nrow<rowl && ncol<coll && !visited[nrow][ncol]){
+                    mat[nrow][ncol]=level+1;
+                    q.offer(new pair(nrow,ncol,level+1));
+                    visited[nrow][ncol]=true;
+                }
+            }
+        }
+        return mat;
+    }
+}
