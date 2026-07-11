b487af4efa34f103ccf8eced2c12bb553382e130
Shashank Rai <164053591+Shashankrai30@users.noreply.github.com>
2026-07-11 23:05:41 +0530

    Time: 8 ms (99.55%), Space: 50.4 MB (19.69%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,47 +1,43 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        long start=0;
-        long end=0;
-
-        for(int i=0;i<weights.length;i++){
-            start=Math.max(start,weights[i]);
-            end=end+weights[i];
+        int i=0;
+        int j=0;
+        for(int w:weights){
+            j+=w;
         }
 
-        long ans=end;
+        while(i<=j){
+            int mid=i+(j-i)/2;
 
-        while(start<=end){
-            long mid=start+(end-start)/2;
+            int cap=mid;
+            int count=0;
+            boolean limit=false;
 
-            long d=helper(weights,mid);          
+            for(int w:weights){
+                if(w>mid){
+                    i=mid+1;
+                    limit = true;
+                    break;
+                }
+                if(cap-w>=0){
+                    cap-=w;
+                }
+                else{
+                    count++;
+                    cap=mid-w;
+                }
+            }
 
-            if(d<=days){
-                ans=Math.min(ans,mid);
-                end=mid-1;
+            if(limit) continue;
+
+            if(count>=days){
+                i=mid+1;
             }
             else{
-                start=mid+1;
+                j=mid-1;
             }
         }
 
-        return (int)ans;
-    }
-
-    long helper(int weights[],long mid){
-
-        long day=1;
-        long total=mid;
-
-        for(int i=0;i<weights.length;i++){
-            if(weights[i]>total){
-                day++;
-                total=mid-weights[i];
-            }
-            else{
-                total=total-weights[i];
-            }
-        }
-
-        return day;
+        return i;
     }
 }
