856c52bda9d139ff75197a5dc9f90353e2bc4f79
Shashank Rai <164053591+Shashankrai30@users.noreply.github.com>
2026-07-14 20:14:26 +0530

    Time: 3 ms (87.71%), Space: 47.8 MB (32.34%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,26 +1,32 @@
 class Solution {
-    public int candy(int[] arr) {
-        int ans[]=new int[arr.length];
+    public int candy(int[] ratings) {
         
-        Arrays.fill(ans,1);
-        
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i]<arr[i+1]){
-                ans[i+1]=ans[i]+1;
+        int pre[] = new int[ratings.length];
+        int suf[] = new int[ratings.length];
+
+        Arrays.fill(pre,1);
+        Arrays.fill(suf,1);
+
+        for(int i=1;i<pre.length;i++){
+            if(ratings[i-1]<ratings[i]){
+                pre[i]=pre[i-1]+1;
             }
         }
-        
-        for(int i=arr.length-1;i>0;i--){
-            if(arr[i]<arr[i-1] && ans[i-1]<=ans[i]){
-                ans[i-1]=ans[i]+1;
+
+        for(int i=pre.length-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                suf[i]=suf[i+1]+1;
             }
+           
         }
-        
+
         int count=0;
-        for(int num:ans){
-            count=count+num;
+
+        for(int i=0;i<ratings.length;i++){
+            count+=Math.max(pre[i],suf[i]);
         }
-        
+
         return count;
+
     }
 }
