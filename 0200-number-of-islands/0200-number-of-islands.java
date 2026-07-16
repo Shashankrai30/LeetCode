5c8492988e09205fe16112e26588836e85c989ba
Shashank Rai <164053591+Shashankrai30@users.noreply.github.com>
2026-07-16 15:56:12 +0530

    Time: 4 ms (46.39%), Space: 52.2 MB (68.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,19 +1,19 @@
 class Solution {
-    int dr[]={1,-1,0,0};
-    int dc[]={0,0,1,-1};
     int rowl;
     int coll;
+    int dr[]={1,-1,0,0};
+    int dc[]={0,0,1,-1};
     public int numIslands(char[][] grid) {
         rowl=grid.length;
         coll=grid[0].length;
-        boolean visited[][]=new boolean[rowl][coll];
+        boolean visited[][] = new boolean[rowl][coll];
 
         int count=0;
 
         for(int i=0;i<rowl;i++){
             for(int j=0;j<coll;j++){
                 if(grid[i][j]=='1' && !visited[i][j]){
-                    helper(i,j,grid,visited);
+                    dfs(i,j,visited,grid);
                     count++;
                 }
             }
@@ -22,15 +22,16 @@ class Solution {
         return count;
     }
 
-    void helper(int row,int col,char grid[][],boolean visited[][]){
+    void dfs(int row,int col,boolean visited[][],char grid[][]){
         visited[row][col]=true;
 
         for(int i=0;i<4;i++){
+
             int nrow=row+dr[i];
             int ncol=col+dc[i];
 
-            if(nrow>=0 && ncol>=0 && nrow<rowl && ncol<coll && grid[nrow][ncol]=='1' && !visited[nrow][ncol]){
-                helper(nrow,ncol,grid,visited);
+            if(nrow>=0 && ncol>=0 && nrow<rowl && ncol<coll && grid[nrow][ncol]=='1' && !visited[nrow][ncol] ){
+                dfs(nrow,ncol,visited,grid);
             }
         }
     }
