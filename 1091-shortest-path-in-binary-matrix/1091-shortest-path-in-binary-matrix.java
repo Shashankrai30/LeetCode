7cf0f6d9467fb2999b3d478f0cbe1437a43e753a
Shashank Rai <164053591+Shashankrai30@users.noreply.github.com>
2026-07-18 23:58:57 +0530

    Time: 29 ms (15.3%), Space: 47.7 MB (58.92%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -2,52 +2,54 @@ class Solution {
     class pair{
         int row;
         int col;
-        int dist;
+        int step;
         public pair(int row,int col,int dist){
             this.row=row;
             this.col=col;
-            this.dist=dist;
+            this.step=dist;
         }
     }
-    int dr[]={1,-1,0,0,1,1,-1,-1};
-    int dc[]={0,0,1,-1,1,-1,1,-1};
+
     int rowl;
     int coll;
+    int dr[] = {1,-1,0,0,1,1,-1,-1};
+    int dc[] = {0,0,1,-1,-1,1,-1,1};
 
     public int shortestPathBinaryMatrix(int[][] grid) {
+
+        if(grid[0][0]!=0) return -1;
+        
         rowl=grid.length;
         coll=grid[0].length;
 
-        boolean visited[][]=new boolean[rowl][coll];
+        int dist[][] = new int[rowl][coll];
 
-        if(grid[0][0]==1) return -1;
+        for(int arr[]:dist){
+            Arrays.fill(arr,Integer.MAX_VALUE);
+        }
 
-        Queue<pair> q= new LinkedList<>();
+        PriorityQueue<pair> pq = new PriorityQueue<>((a,b)-> a.step-b.step);
 
-        q.offer(new pair(0,0,1));
-        visited[0][0]=true;
+        pq.offer(new pair(0,0,1));
+        dist[0][0] = 1;
 
-        while(!q.isEmpty()){
-            pair curr=q.poll();
-
-            int r=curr.row;
-            int c=curr.col;
-            int dist=curr.dist;
-
-            if(r==rowl-1 && c==coll-1){
-                return dist;
-            }
+        while(!pq.isEmpty()){
+            pair curr=pq.poll();
+            int row=curr.row;
+            int col=curr.col;
+            int step=curr.step;
 
             for(int i=0;i<8;i++){
-                int nrow=r+dr[i];
-                int ncol=c+dc[i];
+                int nrow=row+dr[i];
+                int ncol=col+dc[i];
 
-                if(nrow>=0 && ncol>=0 && nrow<rowl && ncol<coll && grid[nrow][ncol]==0 && !visited[nrow][ncol]){
-                    visited[nrow][ncol]=true;
-                    q.offer(new pair(nrow,ncol,dist+1));
+                if(nrow>=0 && ncol>=0 && nrow<rowl && ncol<coll && grid[nrow][ncol]==0 && dist[nrow][ncol]>step+1){
+                    pq.offer(new pair(nrow,ncol,step+1));
+                    dist[nrow][ncol]=step+1;
                 }
             }
         }
-        return -1;
+
+        return (dist[rowl-1][coll-1]==Integer.MAX_VALUE)?-1:dist[rowl-1][coll-1];
     }
 }
