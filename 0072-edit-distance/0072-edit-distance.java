718b03375781f7696ba94a6d966e16bec66602f2
Shashank Rai <164053591+Shashankrai30@users.noreply.github.com>
2026-08-01 15:28:22 +0530

    Time: 3 ms (98.21%), Space: 47.1 MB (71.86%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,13 +1,11 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        Integer dp[][] =new Integer[word1.length()+1][word2.length()+1];
+
+        Integer dp[][] = new Integer[word1.length()][word2.length()];
         return helper(0,0,word1,word2,dp);
     }
 
     int helper(int i,int j,String word1,String word2,Integer dp[][]){
-        if(i==word1.length() && j==word2.length()){
-            return 0;
-        }
 
         if(i==word1.length()){
             return word2.length()-j;
@@ -23,10 +21,10 @@ class Solution {
             return dp[i][j] = helper(i+1,j+1,word1,word2,dp);
         }
 
-        int insert=helper(i,j+1,word1,word2,dp);
-        int delete=helper(i+1,j,word1,word2,dp);
-        int replace=helper(i+1,j+1,word1,word2,dp);
+        int insert=1+helper(i,j+1,word1,word2,dp);
+        int delete=1+helper(i+1,j,word1,word2,dp);
+        int replace=1+helper(i+1,j+1,word1,word2,dp);
 
-        return dp[i][j] = 1 + Math.min(insert,Math.min(delete,replace));
+        return dp[i][j] = Math.min(insert,Math.min(delete,replace));
     }
 }
