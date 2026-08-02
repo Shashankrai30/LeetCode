e05b3c7123e53293c2d4fb3c326f6e182789f5b6
Shashank Rai <164053591+Shashankrai30@users.noreply.github.com>
2026-08-02 19:38:55 +0530

    Time: 103 ms (5.36%), Space: 117.1 MB (8.79%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,35 +1,33 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)-> a[0]-b[0]);
-
-        Integer dp[] = new Integer[intervals.length];
-
-        return intervals.length-helper(0,intervals,dp);
+       Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+       Integer dp[] = new Integer[intervals.length];
+       return intervals.length - helper(0,intervals,dp);
     }
 
-    int helper(int ind,int intervals[][],Integer dp[]){
+    int helper(int i,int intervals[][], Integer dp[]){
 
-        if(ind==intervals.length) return 0;
+        if(i==intervals.length) return 0;
 
-        if(dp[ind]!=null) return dp[ind];
+        if(dp[i]!=null) return dp[i];
 
-        int i=ind;
-        int j=intervals.length-1;
+        int start=i+1;
+        int end=intervals.length-1;
 
-        while(i<=j){
-            int mid=i+(j-i)/2;
+        while(start<=end){
+            int mid=start+(end-start)/2;
 
-            if(intervals[mid][0]>=intervals[ind][1]){
-                j=mid-1;
+            if(intervals[mid][0]>=intervals[i][1]){
+                end=mid-1;
             }
             else{
-                i=mid+1;
+                start=mid+1;
             }
         }
 
-        int take= 1+helper(i,intervals,dp);
-        int skip=helper(ind+1,intervals,dp);
+        int take = 1 + helper(start,intervals,dp);
+        int skip = helper(i+1,intervals,dp);
 
-        return dp[ind] = Math.max(take,skip);
+        return dp[i] = Math.max(take,skip);
     }
 }
